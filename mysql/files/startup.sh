80400f133f7ec4ba73c9094d0937928951890cf0
李尔摩斯 <lcx165@gmail.com>
2016-10-09 14:02:14 +0800

    增加设置步长环境变量

--- mysql/files/startup.sh
+++ mysql/files/startup.sh
@@ -10,5 +10,14 @@ if [ ! -z "$MYSQL_SERVER_ID" ]; then
 	sed -i 's/server-id=.*/server-id='$MYSQL_SERVER_ID'/g' /etc/my.cnf
 fi
 
+
+# 设置自动ID步长
+if [ ! -z "$MYSQL_AUTO_INCREMENT_INCREMENT" ]; then
+	sed -i 's/auto_increment_increment=.*/auto_increment_increment='$MYSQL_AUTO_INCREMENT_INCREMENT'/g' /etc/my.cnf
+fi
+if [ ! -z "$MYSQL_AUTO_INCREMENT_OFFSET" ]; then
+	sed -i 's/auto_increment_offset=.*/auto_increment_offset='$MYSQL_AUTO_INCREMENT_OFFSET'/g' /etc/my.cnf
+fi
+
 # 启动服务
 /usr/bin/mysqld_safe
